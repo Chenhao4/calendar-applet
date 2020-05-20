0a020693ad94542f6e36e7ade4e3b5e2c9a458f2
chenhao <chenhao9730@adpanshi.com>
2020-05-20 09:43:14 +0800

    add authorize

--- src/pages/myInfo/myInfo.jsx
+++ src/pages/myInfo/myInfo.jsx
@@ -1,12 +1,22 @@
 import Taro, { Component } from '@tarojs/taro'
-import { View } from '@tarojs/components'
+import { View, Button, Image } from '@tarojs/components'
 
 export default class Index extends Component {
-  state = { }
+  state = { 
+    nickName: '',
+    avatarUrl: ''
+  }
 
   componentWillMount () { }
 
-  componentDidMount () { }
+  componentDidMount () { 
+    Taro.login({
+      success() {
+        console.log('taro login success')
+      }
+    })
+
+  }
 
   componentWillUnmount () { }
 
@@ -18,12 +28,39 @@ export default class Index extends Component {
     navigationBarTitleText: '我的'
   }
 
+  // 获取微信授权个人信息
+  onGetUserInfo = () => {
+    Taro.getSetting({
+      success: (res) => {
+        if(res.authSetting['scope.userInfo']) {
+          Taro.getUserInfo({
+            success: (userInfoRes) => {
+              console.log(userInfoRes.userInfo)
+              this.setState({
+                nickName: userInfoRes.userInfo.nickName,
+                avatarUrl: userInfoRes.userInfo.avatarUrl
+              })
+            }
+          })
+        } else {
+          console.log('未授权')
+        }
+      }
+    })
+  }
+
   render () {
 
 
     return (
       <View>
         My Info
+        <Button openType='getUserInfo' onClick={this.onGetUserInfo}>点我授权个人信息</Button>
+      <View>
+        {this.state.nickName}
+        <Image src={this.state.avatarUrl} />
+      </View>
+      
         </View>
 
     )
